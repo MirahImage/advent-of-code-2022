70755a53a7566699dcb1e1fb41305d51d2400124
Mirah Gary <mgary@vmware.com>
2022-12-02 15:32:35 +0100

    Refactor to use sum.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,9 +6,8 @@ fn main() {
     let elves = day1::lines_to_elves(read_lines("./input").unwrap());
     let mut calories_list: Vec<i32> = elves.iter().map(|elf| { day1::total_calories(elf) }).collect();
     calories_list.sort();
-    let length=calories_list.len();
-    println!{"The elf with the most calories has {} calories.", calories_list[length - 1]}
-    println!{"The top three elves are carrying {} calories.", calories_list[length - 1] + calories_list[length - 2] + calories_list[length - 3]}
+    println!{"The elf with the most calories has {} calories.", calories_list[calories_list.len()-1]}
+    println!{"The top three elves are carrying {} calories.", calories_list.iter().rev().take(3).sum::<i32>()}
 }
 
 pub fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
