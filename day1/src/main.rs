15f08c2cff9852b63262d1f6ade29fb2063281d9
Mirah Gary <mgary@vmware.com>
2022-12-02 15:25:35 +0100

    Solve day 1 part 2.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,7 +6,9 @@ fn main() {
     let elves = day1::lines_to_elves(read_lines("./input").unwrap());
     let mut calories_list: Vec<i32> = elves.iter().map(|elf| { day1::total_calories(elf) }).collect();
     calories_list.sort();
-    println!{"The elf with the most calories has {} calories.", calories_list[calories_list.len() - 1]}
+    let length=calories_list.len();
+    println!{"The elf with the most calories has {} calories.", calories_list[length - 1]}
+    println!{"The top three elves are carrying {} calories.", calories_list[length - 1] + calories_list[length - 2] + calories_list[length - 3]}
 }
 
 pub fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
